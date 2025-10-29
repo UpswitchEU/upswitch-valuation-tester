de62d2d06743f3689460593e318a4826d59996b3
mandiau-matthias <mmandiau@gmail.com>
2025-10-29 07:56:32 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
@@ -14,7 +14,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:3002',
         changeOrigin: true,
         secure: false,
       },
