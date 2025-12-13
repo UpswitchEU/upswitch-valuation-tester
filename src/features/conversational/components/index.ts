76b214a67288aa39bb1e4b0d5f4b12d5efc4c4ad
mandiau-matthias <mmandiau@gmail.com>
2025-12-13 17:49:21 -0300

    .

---
+++ src/features/conversational/components/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Conversational Components - Unified Export
+ *
+ * @module features/conversational/components
+ */
+
+export { ConversationalLayout } from './ConversationalLayout'
+export { ConversationPanel } from './ConversationPanel'
+export { ReportPanel } from './ReportPanel'
+export { BusinessProfileSection } from './BusinessProfileSection'
+
+export type { ConversationPanelProps } from './ConversationPanel'
+export type { ReportPanelProps } from './ReportPanel'
---
+++ src/features/conversational/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Conversational Feature - Unified Export
+ *
+ * @module features/conversational
+ */
+
+export * from './components'
+export * from './context'
