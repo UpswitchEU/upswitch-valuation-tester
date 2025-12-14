a3047125a59e000cb5f82a287012abfa7a042ce6
mandiau-matthias <mmandiau@gmail.com>
2025-12-14 14:11:50 -0300

    .

---
+++ src/components/results/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Results Component Export
+ * 
+ * Re-exports the Results component from Results.tsx
+ * This ensures proper module resolution in webpack/Vercel builds
+ */
+
+export { Results } from './Results';
