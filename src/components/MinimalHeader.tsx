9f570156001d98c4fc4eef40b1d9bbd97d7a0488
mandiau-matthias <mmandiau@gmail.com>
2025-12-16 07:22:27 -0300

    Update MinimalHeader.tsx

--- src/components/MinimalHeader.tsx
+++ src/components/MinimalHeader.tsx
@@ -78,7 +78,6 @@ export const MinimalHeader: React.FC = () => {
                   objectFit: 'contain',
                   visibility: 'visible',
                   display: 'block',
-                  transform: 'translateY(-3px)',
                 }}
               />
               {/* Hover logo (dark variant) - desktop only */}
