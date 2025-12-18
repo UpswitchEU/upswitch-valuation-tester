2fb71fa0351dc7269b63a089e994e8382155a07d
mandiau-matthias <mmandiau@gmail.com>
2025-12-18 16:42:59 -0300

    Update index.ts

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -6,3 +6,4 @@ export { default as CustomInputField } from './CustomInputField'
 export { default as CustomNumberInputField } from './CustomNumberInputField'
 export { default as CustomTextarea } from './CustomTextarea'
 export { default as HistoricalDataInputs } from './HistoricalDataInputs'
+
